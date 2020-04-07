78cad1f4e15a7722e5230fe8302d45c748688f26
Stephan Philips <s.g.j.philips@tudelft.nl>
2020-04-07 22:40:21 +0200

    added setup and initial file structure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+
+
+packages = ['psb_pulses']
+
+setup(name="pulse_templates",
+	version="0.1",
+	packages = packages,
+)
