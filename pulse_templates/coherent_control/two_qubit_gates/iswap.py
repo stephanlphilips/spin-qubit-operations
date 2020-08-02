928ec71f9f77aacab25d5ec7951579b813bbf9da
Stephan Philips <s.g.j.philips@tudelft.nl>
2020-08-02 17:45:47 +0200

    update on two qubit gates

--- pulse_templates/coherent_control/two_qubit_gates/iswap.py
+++ pulse_templates/coherent_control/two_qubit_gates/iswap.py
@@ -3,20 +3,27 @@ from pulse_templates.utility.oper import add_block, add_ramp
 
 
 @template_wrapper
-def cphase_basic(segment, gates, v_exchange_pulse_off, v_exchange_pulse_on, t_gate, t_ramp):
+def iswap_basic(segment, gates, barrier_gate,v_exchange_pulse_off, v_exchange_pulse_on, v_ac, f_ac, t_gate, t_ramp, padding = 2):
     '''
     basic cphase, with a linear ramp
 
     Args:
         segment (segment_container) : segment to which to add this stuff
-        barrier (str) : barrier to pulse
+        gates (tuple<str>) : gates to be pulses for this gate.
+        barrier_gate (str) : barrier to pulse (for the ac)
         v_exchange_pulse (double) : voltage to pulse to
         t_gate (double) : total time of the gate not inclusing the ramps
         t_ramp (double) : ramp time
     '''
-
     add_ramp(segment, t_ramp, gates, v_exchange_pulse_off, v_exchange_pulse_on)
-    add_block(segment, t_gate, gates, v_exchange_pulse_on)
+    
+    add_block(segment, padding, gates, v_exchange_pulse_on)
+    for gate, level in zip(gates, v_exchange_pulse_on):
+        getattr(segment, gate).add_block(0, t_gate, level)
+    getattr(segment, barrier_gate).add_sin(0, t_gate, v_ac, f_ac)
+    segment.reset_time()
+    add_block(segment, padding, gates, v_exchange_pulse_on)
+
     add_ramp(segment, t_ramp, gates, v_exchange_pulse_on, v_exchange_pulse_off)
 
 
@@ -32,5 +39,5 @@ if __name__ == '__main__':
     base_level = (0,0,0)
     # seg.vP4 += 10
     wait(seg, gates, 100, base_level)
-    cphase_basic(seg, gates, (0,4,0), (0,8,0), 100, 10)
+    iswap_basic(seg, gates, 'vB1' ,(0,4,0), (0,8,0), 2, 1e8, 100, 10)
     plot_seg(seg)   
