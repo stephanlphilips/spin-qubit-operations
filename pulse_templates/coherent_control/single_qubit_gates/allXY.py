df9453723e3e3d7ac5940362f78361579862ace3
Stephan Philips <s.g.j.philips@tudelft.nl>
2021-03-08 08:46:52 +0100

    small fixes

--- pulse_templates/coherent_control/single_qubit_gates/allXY.py
+++ pulse_templates/coherent_control/single_qubit_gates/allXY.py
@@ -1,27 +1,30 @@
 from pulse_lib.segments.utility.looping import loop_obj, linspace
 import numpy as np
 
-class ALLXY:
-    allXY_set = 2*[['I', 'I'], ['X2', 'X2'], ['Y2', 'Y2'], ['X2', 'Y2'], ['Y2', 'X2'], ['X', 'I'], ['Y', 'I'], ['X', 'Y'], ['X', 'Y'], ['X', 'Y2'], ['Y', 'X2'], ['X2', 'Y'], ['Y2', 'X'], ['X', 'X2'], ['X2', 'X'], ['Y', 'Y2'], ['Y2', 'Y'], ['X2', 'I'], ['Y2', 'I'], ['X', 'X'], ['Y', 'Y']]
+def get_allXY_specs(repeat):
+    allXY_set = repeat * [['I', 'I'], ['X2', 'X2'], ['Y2', 'Y2'], ['X2', 'Y2'], ['Y2', 'X2'], ['X', 'I'], ['Y', 'I'], ['X', 'Y'], ['X', 'Y'], ['X', 'Y2'], ['Y', 'X2'], ['X2', 'Y'], ['Y2', 'X'], ['X', 'X2'], ['X2', 'X'], ['Y', 'Y2'], ['Y2', 'Y'], ['X2', 'I'], ['Y2', 'I'], ['X', 'X'], ['Y', 'Y']]
     setpoint = loop_obj()
     
     setpoint.add_data(np.linspace(1,len(allXY_set), len(allXY_set)), axis=0, labels = 'ALL XY gate id', units='#')
+    return allXY_set, setpoint
 
-
-def generate_all_XY(segment, gate_set, axis=0):
+def generate_all_XY(segment, gate_set, repeat = 1, axis=0):
     '''
     generate allXY experiment
 
     Args:
         segment (segment_container) : container of the segments
         gate_set (single_qubit_gate_spec) : object containg instruction of the gate set
+        repeat (int) : the number of time to repeat the allxy experiment =
     '''
-    ALLXY.setpoint.axis = [axis]
 
-    getattr(segment, gate_set.qubit).update_dim(ALLXY.setpoint)
+    allXY_set, setpoint = get_allXY_specs(repeat)
+    setpoint.axis = [axis]
 
-    for i in range(ALLXY.setpoint.data.size):
-        gate_set.load_std_gate(getattr(segment, gate_set.qubit)[i], ALLXY.allXY_set[i])
+    getattr(segment, gate_set.qubit).update_dim(setpoint)
+
+    for i in range(setpoint.data.size):
+        gate_set.load_std_gate(getattr(segment, gate_set.qubit)[i], allXY_set[i])
 
 if __name__ == '__main__':
     from pulse_templates.coherent_control.single_qubit_gates.standard_set import single_qubit_std_set
@@ -29,13 +32,14 @@ if __name__ == '__main__':
     from pulse_templates.demo_pulse_lib.virtual_awg import get_demo_lib
     from pulse_templates.utility.plotting import plot_seg
     from pulse_lib.segments.utility.looping import linspace
-    pulse = get_demo_lib('quad')
+    pulse = get_demo_lib('six')
     seg = pulse.mk_segment()
-    xpi2 = single_qubit_gate_spec('qubit4_MW', 1e9+ linspace(0,50,20, axis=0), 100, 120)
-    xpi = single_qubit_gate_spec('qubit4_MW', 1e9+ linspace(0,50,20, axis=0), 200, 120)
-    ss_set = single_qubit_std_set()
-    ss_set.X = xpi2
-    ss_set.X2 = xpi
 
-    generate_all_XY(seg, ss_set,0)
-    # plot_seg(seg, [1])
+    ss_set = single_qubit_std_set()
+    ss_set.X =  single_qubit_gate_spec('qubit4_MW', 1e9, 100, 120)
+    # ss_set.X2 = single_qubit_gate_spec('qubit4_MW', 1e9, 200, 120)
+
+    # ss_set.X.add(seg)
+    print(seg)
+    generate_all_XY(seg, ss_set)
+    plot_seg(seg, 1)
